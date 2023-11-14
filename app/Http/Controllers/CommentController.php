f7d4323922742c6a4ccbde06c98f42d92a904479
VSL <popart.vl@gmail.com>
2023-11-14 15:32:59 +0700

    Refactor controllers and add form requests

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,23 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateCommentRequest;
 use App\Models\Comment;
 use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function store(Idea $idea){
-        // $validated = request()->validate([
-        //     'content' => 'required|min:3|max:240'
-        // ]);
+    public function store(CreateCommentRequest $request ,Idea $idea)
+    {
+        $validated = $request->validated();
 
-        $comment = new Comment();
-        $comment->idea_id = $idea->id;
-        $comment->user_id = auth()->id();
-        $comment->content = request()->get('content');
-        $comment->save();
+        $validated['user_id'] = auth()->id();
+        $validated['idea_id'] = $idea->id;
 
-        return redirect()->route('ideas.show',$idea->id)->with('success', "Comment posted successfully!");
+        Comment::create($validated);
+
+
+        return redirect()->route('ideas.show', $idea->id)->with('success', "แสดงความคิดเห็นเรียบร้อย!");
     }
 }
