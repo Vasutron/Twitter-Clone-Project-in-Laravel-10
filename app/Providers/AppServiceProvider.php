91c0a68c4627c2d18cf899671130bdb3390db789
VSL <popart.vl@gmail.com>
2024-04-23 17:00:47 +0700

    Refactor caching logic in AppServiceProvider.php and update topUsers variable #46

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,10 +26,21 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
+        // \Debugbar::enable();
+
+        // cache()->forget('topUsers');
+
+        // Cache::forget('topUsers');
+
+        $topUsers = cache()->remember('topUsers', now()->addMinute(5), function () {
+            return User::withCount('ideas')
+                ->orderBy('ideas_count', 'DESC')
+                ->take(10)->get();
+        });
+
         View::share(
             'topUsers',
-            User::withCount('ideas')
-                ->orderBy('ideas_count', 'DESC')
-                ->limit(5)->get());
+            $topUsers
+        );
     }
 }
